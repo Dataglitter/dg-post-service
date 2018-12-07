00c16e5dac1e29856cb401172ed767a73e08d805
Sangram Reddy <reddy.horcrux@gmail.com>
2018-12-07 12:39:29 +0000

    Minor refactoring

--- scripts/button.sh
+++ scripts/button.sh
@@ -3,4 +3,5 @@ sleep 60
 java -jar /home/app/PostService-$SERVICE_VERSION.jar \
     --spring.data.mongodb.host=$MONGO \
     --server.ssl.key-store-password=$KEY_STORE_PASSWORD \
-    --server.ssl.key-password=$KEY_PASSWORD
+    --server.ssl.key-password=$KEY_PASSWORD \
+    --server.jwt.secret=$SECRET
